53e35ea35f3172f10cfcec7862f4b2d95e1a7d34
Tomi <Tomi@DESKTOP-8F1FU4F>
2026-04-14 20:22:05 +0200

    add auth api, delete booking test

---
+++ api/auth.api.ts
@@ -0,0 +1,17 @@
+import { APIRequestContext } from '@playwright/test';
+
+export class AuthApi {
+  constructor(private request: APIRequestContext) {}
+
+  async getToken(): Promise<string> {
+    const response = await this.request.post('/auth', {
+      data: {
+        username: 'admin',
+        password: 'password123',
+      },
+    });
+
+    const body = await response.json();
+    return body.token;
+  }
+}
--- tests/api/booking.api.spec.ts
+++ tests/api/booking.api.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect, request } from '@playwright/test';
+import { AuthApi } from '../../api/auth.api';
 import { BookingFactory } from '../../factories/booking.factory';
 
-test('POST /booking - create booking', async () => {
+test('POST /booking - create & delete booking', async () => {
   const apiContext = await request.newContext({
     baseURL: 'https://restful-booker.herokuapp.com',
   });
@@ -27,4 +28,14 @@ test('POST /booking - create booking', async () => {
   expect(getBody.lastname).toBe(booking.lastname);
   expect(getBody.bookingdates.checkin).toBe(booking.bookingdates.checkin);
   expect(getBody.bookingdates.checkout).toBe(booking.bookingdates.checkout);
+
+  const authAPI = new AuthApi(apiContext);
+  const token = await authAPI.getToken();
+
+  const deleteResponse = await apiContext.delete(`/booking/${bookingId}`, {
+    headers: {
+      Cookie: `token=${token}`,
+    },
+  });
+  expect(deleteResponse.status()).toBe(201);
 });
